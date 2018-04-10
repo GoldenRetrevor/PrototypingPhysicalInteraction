789370243bbdf45ce6ab1432f8539e7ba456a4f2
GoldenRetrevor <toantu3005@gmail.com>
2018-04-10 20:21:41 +1000

    Add files via upload

--- ToneShake.ino
+++ ToneShake.ino
@@ -1,19 +1,24 @@
 
 #include "pitches.h"
 
-const int buzzer = 8; // Buzzer connected to pin 8
-int sensor; // Sensor connected to pin 10
+const int buzzer = 4; // Buzzer connected to D4
 
-// notes in the melody
-int melodys[3][8] = {
-  {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4},
-  {NOTE_AS1, NOTE_E2, NOTE_CS1, NOTE_C5, NOTE_E6, 0, NOTE_FS5, NOTE_G2},
-  {NOTE_D7, NOTE_A5, NOTE_DS6, NOTE_B6, NOTE_A4, 0, NOTE_FS3, NOTE_DS2}
+// melodies and notes
+int melody1[8] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+int melody2[8] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4, NOTE_AS3, NOTE_AS4, 0, 0
+};
+
+int melody3[8] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7, 0, NOTE_C7, NOTE_E7, 0
 };
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
+  12, 12, 12, 12, 12, 12, 12, 12, 12
 };
 
 void setup() {
@@ -22,25 +27,84 @@ void setup() {
   pinMode(buzzer, OUTPUT);
 }
 
+void playMelody1() {
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(buzzer, melody1[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+  }
+  noTone(buzzer);
+}
+
+void playMelody2() {
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(buzzer, melody2[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+  }
+  noTone(buzzer);
+}
+
+void playMelody3() {
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(buzzer, melody3[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+  }
+  noTone(buzzer);
+}
+
+void switchMelody(int m) {
+  if (m == 1) {
+    playMelody1();
+  }
+  else if (m == 2) {
+    playMelody2();
+  }
+  else if (m == 3) {
+    playMelody3();
+  }
+}
+
+int thisMelody = 1;
+
 void loop() {
   // put your main code here, to run repeatedly:
-  sensor = analogRead(A0);
-  if (sensor < 1022) {
-    for (int thisNote = 0; thisNote < 8; thisNote++) {
-  
-      // to calculate the note duration, take one second divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-      int noteDuration = 1000 / noteDurations[thisNote];
-      tone(buzzer, melodys[thisMelody][thisNote], noteDuration);
-  
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
-      int pauseBetweenNotes = noteDuration * 1.30;
-      thisMelody += 1;
-      delay(pauseBetweenNotes);
-    }
+  int sensor = analogRead(A0);  // read vibration value
+  Serial.println(sensor);
+
+  if (thisMelody > 3) {
+    thisMelody = 1;
+  }
+
+  if (sensor > 1022) {
+    switchMelody(thisMelody);
+    thisMelody += 1;
+    delay(1000);
   }
   else { // stop the tone playing
-      noTone(buzzer);
+    noTone(buzzer);
   }
+  noTone(buzzer);
 }
